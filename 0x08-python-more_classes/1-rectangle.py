a3afb288ed21a2e75cebb73a2e5d091ebdd19549
Whitney-Nihi <whitney.taire1@gmail.com>
2024-01-30 05:00:46 -0800

    Define private instance variables

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width(int): The width of the Rectangle
+            height(int): The height of the Rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of the Rectangle"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def heigt(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
