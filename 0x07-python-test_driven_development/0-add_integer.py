d592139f77feb816c2c10e52d1e765bee50a60f7
Whitney-Nihi <whitney.taire1@gmail.com>
2024-02-02 13:59:57 -0800

    Modified if statement

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either a or b is not an integer or float.
     """
-    if type(a) != int and type(a) != float:
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:  
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
