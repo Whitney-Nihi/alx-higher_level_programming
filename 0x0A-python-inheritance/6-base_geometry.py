52405f2d245a52093802699a6ae38e20ac59db72
Whitney-Nihi <whitney.taire1@gmail.com>
2024-02-05 15:58:53 -0800

    Function raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Base Geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
