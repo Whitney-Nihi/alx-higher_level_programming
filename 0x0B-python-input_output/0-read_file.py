c43bf1d9cac42554780e5ca0ca0fea86bc595b88
Whitney-Nihi <whitney.taire1@gmail.com>
2024-02-06 14:38:13 -0800

    Removed end def from last line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,4 @@
 def read_file(filename=""):
     """Function reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+        print(f.read())
