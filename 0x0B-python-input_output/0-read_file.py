2d317e8d231779abe44924483027942196fb8c47
Whitney-Nihi <whitney.taire1@gmail.com>
2024-02-06 14:44:00 -0800

    Added one more line for doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines a function that reads a text file."""
 
+
 def read_file(filename=""):
     """Function reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-        print("\n")
