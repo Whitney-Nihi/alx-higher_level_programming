66b9358157deee515e7c2faf98312867d956622d
Whitney-Nihi <whitney.taire1@gmail.com>
2024-02-06 15:31:20 -0800

    Function appends a string to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that appends a string to a file."""
+
+
+def append_write(filename="", text=""):
+    """Function appends string to end of a file and returns number of characters added.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): Text to be appended to file.
+    Returns:
+        The number of characters added.
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
