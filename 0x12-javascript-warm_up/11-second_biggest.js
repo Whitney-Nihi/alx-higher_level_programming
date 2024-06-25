c7dc33dbd8f38f1a5a7b9e3d8cb8e42e9ab32397
Whitney-Nihi <whitney.taire1@gmail.com>
2024-06-25 15:22:48 -0700

    Modified code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // Searches the second biggest integer in the list of arguments
 
 if (process.argv.length <= 3) {
-  console.log('0');
+  console.log(parseInt(0));
 } else {
   const list = process.argv.sort();
   console.log(list.reverse()[1]);
