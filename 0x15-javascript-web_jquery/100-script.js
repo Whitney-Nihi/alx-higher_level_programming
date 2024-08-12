74b90073f90701213227ae0da6f75d5a177787ca
Whitney-Nihi <whitney.taire1@gmail.com>
2024-08-12 16:59:33 -0700

    Script updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Wait for the DOM to fully load
+document.addEventListener('DOMContentLoaded', () => {
+  // Select the <header> element
+  const header = document.querySelector('header');
+  // Update the text color to red
+  header.style.color = '#FF0000';
+});
